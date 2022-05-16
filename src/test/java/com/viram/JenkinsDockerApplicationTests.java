3a4512c0040a27ef4ec807ee979005a0207a21c1
Viram Dhangar <viram.dhangar@gmail.com>
2022-05-16 09:01:59 +0530

    first commit

---
+++ src/test/java/com/viram/JenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
